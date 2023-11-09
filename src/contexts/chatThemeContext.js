559e86b66353f9597259dc9e6fff63e9eb8b1694
여다희 <yeodahui.dev@gmail.com>
2023-11-10 08:13:14 +0900

    추가: 채팅 테마 Provider 사용 및 defaultTheme 객체 별도 저장

--- src/contexts/chatThemeContext.js
+++ src/contexts/chatThemeContext.js
@@ -7,16 +7,17 @@ export const useChatThemeContext = () => {
 };
 
 export const ChatThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState({
+  const defaultTheme = {
     background: "#B2C7D9",
     bubbleBg: "#EEEEEE",
     bubbletext: "#222222",
     myBubbleBg: "#FFDC00",
     myBubbleText: "#222222",
-  });
+  };
+  const [theme, setTheme] = useState(defaultTheme);
 
   return (
-    <ChatThemeContext.Provider value={{ theme, setTheme }}>
+    <ChatThemeContext.Provider value={{ defaultTheme, theme, setTheme }}>
       {children}
     </ChatThemeContext.Provider>
   );
--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ChatProvider } from "./contexts/chatContext";
+import { ChatThemeProvider } from "./contexts/chatThemeContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChatProvider>
-      <App />
+      <ChatThemeProvider>
+        <App />
+      </ChatThemeProvider>
     </ChatProvider>
   </React.StrictMode>
 );
