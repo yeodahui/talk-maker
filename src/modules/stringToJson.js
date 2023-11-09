042bb560a544c9a7cc8d159e93b2306a61f1d573
여다희 <yeodahui.dev@gmail.com>
2023-11-10 01:34:36 +0900

    수정: 채팅 내역에서 user, message 데이터 따옴표 제거해 저장

--- src/modules/stringToJson.js
+++ src/modules/stringToJson.js
@@ -1,6 +1,6 @@
-export const stringToJson = (string_string) => {
+export const stringToJson = (string) => {
   // 1. 문자열을 줄바꿈으로 구분 => 배열에 저장
-  const rows = string_string.split("\n");
+  const rows = string.split("\n");
 
   // 2. 빈 배열 생성: string의 각 행을 담을 JSON 객체임
   const jsonArray = [];
@@ -9,12 +9,21 @@ export const stringToJson = (string_string) => {
   const header = rows[0].split(",");
 
   // 4. 내용 행 전체를 객체로 만들어, jsonArray에 담기
-  for (let i = 1; i < rows.length; i++) {
-    const row = rows[i].split(","); // 각 내용 행을 콤마로 구분
+  for (let i = 1; i < rows.length - 1; i++) {
+    let row = rows[i].split(","); // 각 내용 행을 콤마로 구분
     const obj = {};
     // 각 내용행을 {제목1:내용1, 제목2:내용2, ...} 형태의 객체로 생성
     for (let j = 0; j < header.length; j++) {
-      obj[header[j]] = row[j];
+      let data = String(row[j]);
+      if (j === 0) {
+        // 첫 행은 Date 타입으로 변환
+        data = new Date(data);
+      } else {
+        // 나머지 행 앞뒤 큰따옴표 제거
+        data = data.slice(1, -1);
+      }
+      // obj[header[j]] = row[j];
+      obj[header[j]] = data;
     }
 
     // 각 내용 행의 객체를 jsonArray배열에 담기
