cafc815cfb1cae43b9135027f686c0fbded9d519
여다희 <yeodahui.dev@gmail.com>
2023-11-10 08:16:36 +0900

    추가: chatContext에서 me, user 데이터 관리

--- src/contexts/chatContext.js
+++ src/contexts/chatContext.js
@@ -1,5 +1,5 @@
 // JSON화 된 파일을 전역으로 관리
-import { useContext, createContext, useState } from "react";
+import { useContext, createContext, useState, useEffect } from "react";
 
 const chatContext = createContext();
 
@@ -9,10 +9,67 @@ export const useChatContext = () => {
 
 export const ChatProvider = ({ children }) => {
   const [chat, setChat] = useState();
+  const [me, setMe] = useState();
+  const [users, setUsers] = useState();
 
-  return (
-    <chatContext.Provider value={{ chat, setChat }}>
-      {children}
-    </chatContext.Provider>
-  );
+  useEffect(() => {
+    if (chat === null) {
+      setMe();
+      setUsers();
+      return;
+    }
+
+    // 중복 불가능한 배열 Set에 대화내역의 User 이름 저장
+    const arrUsers = new Set();
+
+    if (chat) {
+      chat.map(({ User }) => {
+        if (User !== "undefined") {
+          arrUsers.add(User);
+        }
+      });
+
+      setUsers([...arrUsers]);
+    }
+  }, [chat]);
+
+  const changeUserName = (prevname, newname) => {
+    if (prevname === newname) {
+      return;
+    }
+
+    const index = users.indexOf(prevname);
+    if (index !== -1) {
+      const newUsers = users;
+      newUsers[index] = newname;
+
+      setUsers(newUsers);
+    }
+
+    const newChat = chat.map((item) => {
+      if (item.User === prevname) {
+        item.User = newname;
+      }
+      return item;
+    });
+    console.log(newChat);
+
+    if (me === prevname) {
+      setMe(newname);
+    }
+
+    setChat(newChat);
+  };
+
+  const value = {
+    chat,
+    setChat,
+    me,
+    setMe,
+    changeUserName,
+    users,
+    setUsers,
+  };
+
+  return <chatContext.Provider value={value}>{children}</chatContext.Provider>;
 };
